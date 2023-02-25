2310176657554f81a0fe05e436d474ad2ed11e9e
Elena <Shherbakove@yandex.ru>
2023-02-25 21:12:59 +0300

    ДЗ 5 семинар

---
+++ Task36/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] FillArrayWithRandomNumbers(int length)
+{
+    int[] arr = new int [length];
+    Random rnd = new Random();
+    for(int i =0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(1, 10);
+    }
+    return arr;
+} 
+
+System.Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = FillArrayWithRandomNumbers(length);
+System.Console.WriteLine($"[ {string.Join(", ", array)} ]");
+
+int sumN = 0;
+
+for(int i = 1; i < array.Length; i+=2)
+{
+        sumN+=array[i];
+}
+
+System.Console.WriteLine($"Сумма элементов на нечетных позициях = {sumN}");
---
+++ Task38/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+double[] FillArrayWithRandomNumbers(int length)
+{
+    double[] arr = new double [length];
+    Random rnd = new Random();
+    for(int i =0; i < arr.Length; i++)
+    {
+        arr[i] = Math.Round(new Random().NextDouble() + new Random().Next(0,100), 2);
+    }
+    return arr;
+} 
+
+System.Console.WriteLine("Введите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+double[] array = FillArrayWithRandomNumbers(length);
+System.Console.WriteLine($"[ {string.Join("; ", array)} ]");
+
+double min = array[0];
+double max = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > max)
+        {
+            max = array[i];
+        }
+    if (array[i] < min)
+        {
+            min = array[i];
+        }
+}
+
+Console.WriteLine($"Максимальное значение = {max}, минимальное значение = {min}");
+Console.WriteLine();
+Console.WriteLine($"Разница между максимальным и минимальным значением = {max - min}");
